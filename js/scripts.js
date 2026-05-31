c62e5f207e744d341e8cf1c44c113e1f6ac7fceb
Learning-Development667 <markbrown667@gmail.com>
2026-05-31 16:17:31 +0100

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,7 @@
 // ── CONFIG ──
 const PASSWORD = '!TrainingApp202';
 const BIN_ID   = '6a1c1ce5ddf5aa59f77b7666';
-const API_KEY  = 'YOUR_API_KEY_HERE'; // Paste your JSONBin Master Key here
+const API_KEY  = '$2a$10$3ndQ23/GtXtiDjSQ8iUDOOQ7Qi/0m2u8KKkGmhfz5tJv1hSfb/U2W'; // Paste your JSONBin Master Key here
 const BIN_URL  = `https://api.jsonbin.io/v3/b/${BIN_ID}`;
 
 // ── EXERCISE DEFINITIONS ──
